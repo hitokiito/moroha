13125fbd599da832932925cd3583c17af035c414
pedrozorn <itt.pedrodev53@gmail.com>
2023-03-23 22:24:48 +0900

    日程編集機能追加 #14

--- src/event-utils.ts
+++ src/event-utils.ts
@@ -4,15 +4,26 @@ let eventGrid = 0;
 const todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD
 export const createEventId = () => String(eventGrid++);
 export const INITIAL_EVENTS: EventInput[] = [
-  {
-    id: createEventId(),
-    title: "All-day event",
-    start: todayStr,
-  },
+  // {
+  //   id: createEventId(),
+  //   title: "All-day event",
+  //   start: todayStr,
+  // },
   {
     id: createEventId(),
     title: "Timed event",
     start: todayStr + "T12:00:00",  // 時刻はTで区切る
   },
+  {
+    id: createEventId(),
+    title: todayStr,
+    start: todayStr + "T10:00:00",  // 時刻はTで区切る
+    end: todayStr + "T12:00",  // 時刻は
+  },
+  // {
+  //   id: createEventId(),
+  //   title: "Timed event",
+  //   start: "2023-03-24",  // 時刻はTで区切る
+  // },
 
 ]
