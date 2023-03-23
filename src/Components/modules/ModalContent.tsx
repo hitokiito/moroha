0136f942282dc8017a8b6fe32a6da89b66d03628
pedrozorn <itt.pedrodev53@gmail.com>
2023-03-23 19:45:39 +0900

    削除機能仮実装

--- src/Components/modules/ModalContent.tsx
+++ src/Components/modules/ModalContent.tsx
@@ -1,5 +1,6 @@
-import { EventClickArg } from "@fullcalendar/core";
+import { EventApi, EventClickArg } from "@fullcalendar/core";
 import React,{ useState,useEffect,VFC } from "react";
+import { start } from "repl";
 
 import { GetModalContents } from "../../hooks/useModalContents";
 
@@ -9,12 +10,17 @@ import { GetModalContents } from "../../hooks/useModalContents";
 type Props = {
   // modalId: string;
   content: EventClickArg | undefined
+  currentEvents: EventApi[]
+  setCurrentEvents:React.Dispatch<React.SetStateAction<EventApi[]>>
   // content: EventClickArg;
 };
 
 export const ModalContent: React.VFC<Props> = (props) => {
-
+  
   const content = props.content;
+  const { currentEvents, setCurrentEvents } = props;
+  // console.log(clickInfo.event.id);
+  const [id, setId] = useState("");
   const [title, setTitle] = useState("タイトル");
   const [allDay, setAllDay] = useState(false);
   const [startDate, setStartDate] = useState("startDate");
@@ -24,7 +30,8 @@ export const ModalContent: React.VFC<Props> = (props) => {
 
     // const allDay = modal?.event.allDay;
   useEffect(() => {
-    if (content) {
+    if (content) { 
+      setId(content!.event.id ?? "");
       setTitle(content!.event._def.title ?? "")
       const start = content!.event._instance!.range.start.toISOString() ?? "";
       const end = content!.event._instance!.range.end.toISOString() ?? "";
@@ -39,6 +46,10 @@ export const ModalContent: React.VFC<Props> = (props) => {
   }, [content]);
 
 
+  
+
+
+  
   // TODO エラー回避のため一時的な処理
   if (!content) {
     return (
@@ -48,6 +59,8 @@ export const ModalContent: React.VFC<Props> = (props) => {
   
   // タイトルだけ変更可能にしておく
   const handleChange = (event: any) => {
+    // console.log(event);
+    console.log(currentEvents);
     switch (event.target.name) {
       case 'title':
         setTitle(event.target.value);
@@ -61,15 +74,41 @@ export const ModalContent: React.VFC<Props> = (props) => {
     }
   };
 
-  const handleSubmit = (event: any) => {
-    console.log("提出");
+  // 変更時のイベント作成
+  // 今はそのまま実行される。
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const event = {
+      id: id,
+      title: title,
+      start: "2020-05-23 10:00:00",
+      end: endDate,
+    }
+    deleteEvent(event)
+  };
+  
+  // 削除
+  const deleteEvent = (ev: any) => {
+    console.log("changeEvent通過");
+    console.log("押下対象Id" + ev.id);
+    // カレンダーオブジェクト取得して削除
+    const CalAPI = content.view.calendar
+    console.log(CalAPI.getEventById(ev.id));
+    CalAPI.getEventById(ev.id)!.remove();
+    return true;
   };
 
   return (
     <>
       {content ? (
         <>
-          <form onSubmit={handleSubmit}>
+          <form>
+          {/* <form onSubmit={handleSubmit} onClick={handleSubmit}> */}
+            <label>
+              ID(テスト表示用)
+              <input type="text" name="id" value={id} onChange={handleChange} />
+            </label>
+            <br />
             <label>
               Title:
               <input type="text" name="title" value={title} onChange={handleChange} />
@@ -86,14 +125,9 @@ export const ModalContent: React.VFC<Props> = (props) => {
             </label>
             <br />
             <input type="submit" value="Submit" />
+            <button onClick={handleSubmit}>dejaaaaaaaaaa</button>
           </form>
         
-          {/* <div>タイトル:{title}</div>
-          <div>allDay:{allDay.toString()}</div>
-          <div>開始日: {startDate}</div>
-          <div>開始時間: {startTime}</div>
-          <div>終了日: {endDate}</div>
-          <div>終了時間: {endTime}</div> */}
         </>
       ) : (
         <></>
--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,34 +1,53 @@
-import { VFC,} from "react"
+import { EventApi, formatDate } from "@fullcalendar/core";
 
-
-type Props = {
-  toggleWeekEnds: () => void;
+interface Props {
+  currentEvents: EventApi[];
+  toggleWeekends: () => void;
   weekendsVisible: boolean;
-};
-
-
-export const Sidebar: VFC<Props> = ({
-  toggleWeekEnds,  weekendsVisible
+}
+const renderSidebarEvent = (event: EventApi) => (
+  <li key={event.id}>
+    <p>{event.id}</p>
+    <b>
+      {formatDate(event.start!, {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        locale: "ja",
+      })}
+    </b>
+    <i>{event.title}</i>
+  </li>
+);
+const Sidebar: React.VFC<Props> = ({
+  currentEvents,
+  toggleWeekends,
+  weekendsVisible,
 }) => (
-  <>
-  <div className="app-sidebar">
-    <div className="sidebar-section">
-      <h2>捜査方法</h2>
+  <div className="demo-app-sidebar">
+    <div className="demo-app-sidebar-section">
+      <h2>操作方法</h2>
       <ul>
         <li>日付を選んでイベント作成</li>
+        <li>ドラッグ&amp;ドロップでイベントの長さ変更</li>
         <li>クリックでイベント削除</li>
       </ul>
-      </div>
-      <div className="sidebar-section">
+    </div>
+    <div className="demo-app-sidebar-section">
       <label>
         <input
           type="checkbox"
           checked={weekendsVisible}
-          onChange={toggleWeekEnds}
+          onChange={toggleWeekends}
         />
-         休日を表示
+        週末を表示
       </label>
     </div>
-  </div>;
-  </>
-  )
+    <div className="demo-app-sidebar-section">
+      <h2>予定一覧({currentEvents.length})</h2>
+      <ul>{currentEvents.map(renderSidebarEvent)}</ul>
+    </div>
+  </div>
+);
+
+export default Sidebar;
