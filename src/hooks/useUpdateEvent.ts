a63f4908e00cc2f68a1c9437ed1e9cd26ff129fc
pedrozorn <itt.pedrodev53@gmail.com>
2023-04-03 21:57:42 +0900

    編集実装

--- src/hooks/useUpdateEvent.ts
+++ src/hooks/useUpdateEvent.ts
@@ -2,18 +2,7 @@ import { useEffect, useState } from 'react';
 import { gapi } from 'gapi-script';
 import { EventClickArg } from '@fullcalendar/core';
 
-interface Event {
-  id: string;
-  summary?: string;
-  start?: {
-    dateTime: string;
-  };
-  end?: {
-    dateTime: string;
-  };
-  location?: string;
-  description?: string;
-}
+interface Event extends gapi.client.calendar.Event {}
 
 const useUpdateEvent = (content: EventClickArg | undefined) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -21,43 +10,82 @@ const useUpdateEvent = (content: EventClickArg | undefined) => {
   console.log(content);
 
   useEffect(() => {
-    const initClient = async () => {
+    const loadClient = async () => {
       try {
-        await gapi.client.init({
-          apiKey: '',
-          clientId: '',
-          discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-          scope: 'https://www.googleapis.com/auth/calendar',
+        await gapi.load('client:auth2', () => {
+          console.log('gapi.load');
+
+          gapi.client.init({
+            clientId: '',
+            scope: 'https://www.googleapis.com/auth/calendar',
+          });
+          console.log('gapi.client.init')
+
+          gapi.client.load('calendar', 'v3', () => {
+            console.log('gapi.client.load');
+          });
+          gapi.auth2.init({
+            client_id: '',
+            scope: 'https://www.googleapis.com/auth/calendar',
+          });
+          console.log('gapi.auth2.init');
+
+          gapi.auth2.getAuthInstance().signIn();
+          console.log('gapi.auth2.getAuthInstance().signIn()');
+        
         });
-        console.log('gapi client initialized');
+        const googleUser = await gapi.auth2.getAuthInstance().signIn();
+        const accessToken = googleUser.getAuthResponse().access_token;
+        console.log(accessToken);
+        console.log('accessToken');
+        const tokenObject: GoogleApiOAuth2TokenObject = {
+          access_token: accessToken,
+          expires_in: '3600', // 有効期限(秒)
+          error: '', // エラーが発生しなかった場合はnull
+          state: '' // 状態が存在しない場合はnull
+        };
+        
+        gapi.auth.setToken(tokenObject);
+
+        gapi.client.load('calendar', 'v3', () => {
+          console.log('Calendar API has been loaded successfully.');
+      });
       } catch (error) {
-        console.log('gapi client initialization error', error);
+        console.log('Error occurred while initializing the GoogleAuth.', error);
       }
     };
-    initClient();
+
+    if (typeof gapi !== 'undefined') {
+      loadClient();
+    }
   }, []);
 
   const updateEvent = async (content: EventClickArg | undefined) => {
-    console.log('start')
+    console.log('start');
     console.log('updateEvent');
     setIsLoading(true);
+
     try {
-      const updateObject: Event ={
-        id: ''
+      const updateObject: Event = {
+        id: content!.event.id,
+        summary: content!.event._def.title,
+        start: {
+          date: content!.event._instance!.range.start.toISOString(),
+        },
+        end: {
+          date: content!.event._instance!.range.end.toISOString(),
+        },
       };
 
       if (content!.event.id) {
-        updateObject.id = content!.event.id
+        updateObject.id = content!.event.id;
       }
-
 
       if (content && content.event._def.title) {
         updateObject.summary = content.event._def.title;
       }
-      
 
       if (content!.event._instance!.range.start) {
-        // content!.event._instance!.range.start.toISOString() ?? ""
         updateObject.start = {
           dateTime: content!.event._instance!.range.start.toISOString(),
         };
@@ -69,23 +97,22 @@ const useUpdateEvent = (content: EventClickArg | undefined) => {
         };
       }
 
-      // if (event.location) {
-      //   updateObject.location = event.location;
-      // }
+      const googleAuth = gapi.auth2.getAuthInstance();
+      if (!googleAuth.isSignedIn.get()) {
+        await googleAuth.signIn();
+      }
 
-      // if (event.description) {
-      //   updateObject.description = event.description;
-      // }
+      // const accessToken = googleAuth.currentUser.get().getAuthResponse().access_token;
 
       await gapi.client.calendar.events.update({
-        calendarId: 'itt.pedro5328@gmail.com',
+        calendarId: '',
         eventId: content!.event.id,
         resource: updateObject,
       });
+
       console.log('イベントが更新されました。');
     } catch (error) {
-      console.log('エラー');
-      console.log(error);
+      console.log('エラー', error);
       setError(error as Error);
     } finally {
       setIsLoading(false);
