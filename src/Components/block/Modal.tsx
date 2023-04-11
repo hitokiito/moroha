2be886811e16cd09c74400cdf3fe85dd3beafe6e
pedrozorn <itt.pedrodev53@gmail.com>
2023-04-11 22:14:06 +0900

    モーダル範囲外押下時に閉じる処理追加

--- src/Components/block/Modal.tsx
+++ src/Components/block/Modal.tsx
@@ -8,48 +8,19 @@ import ModalContent from "../modules/ModalContent";
 type Props = {
   modalStatus: boolean;
   content: EventClickArg | undefined;
-  modalOpen: () => void;
+  modalOpen: (event:any) => void;
   modalClose: () => void;
 };
 
 export const Modal: React.VFC<Props> = (props) => {
   const { modalStatus, content, modalOpen, modalClose } = props;
   const insideRef = useRef<HTMLDivElement>(null);
-  // const handleClickOutside = useCallback(
-  //   (e: MouseEvent) => {
-  //     // console.log("handleClickOutside");
-  //     if (!insideRef.current?.contains(e.target as Node)) {
-  //       console.log("hundleClickOutside")
-  //       modalClose()
-  //       //ここに外側をクリックしたときの処理
-  //     } else {
-  //       console.log("hundleClickinside")
-  //       //ここに内側をクリックしたときの処理
-  //     }
-  //   },
-  //   []
-  // );
-  
-  // useEffect(() => {
-  //   if (!modalStatus) return;
-  //   console.log("moldalStatuがtrueできている。");
-  //   //クリックイベントを設定
-  //   document.addEventListener("click", handleClickOutside);
-
-  //   //クリーンアップ関数
-  //   return () => {
-  //     console.log(modalStatus)
-  //     //コンポーネントがアンマウント、再レンダリングされたときにクリックイベントを削除
-  //     document.removeEventListener("click", handleClickOutside);
-  //     console.log("クリーンアップ関数");
-  //   };
-  // }, [modalStatus,handleClickOutside]);
 
   return (
     <div className={modalStatus ? "modal is-active" : "modal"}>
       <div id="overlay">
         <div id="content">
-          <div className="modal-inner" ref={insideRef}>
+          <div className="modal-inner" ref={insideRef} onClick={(event)=>{event.stopPropagation()}}>
             <div className="modal-content">
               <div className="modal-header float-right ">
                 <IconContext.Provider value={{ size: "40px" }}>
